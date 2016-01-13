4c4428583e64e3f5ad860795e14f6fe6dbc15446
gregapompe <grega.pompe@informa.si>
2016-01-13 13:25:45 +0100

    Batch send GUI

--- lib/StoreMail/Routes/GUI.pm
+++ lib/StoreMail/Routes/GUI.pm
@@ -10,18 +10,26 @@ our $VERSION = '0.1';
 prefix '/:domain';
 
 get '/gui/conversation/all' => sub {
+	content_type('text/html');
     return template 'all.html', {domain => param('domain')};
 };
 
 
 get '/gui/conversation/:id' => sub {
+	content_type('text/html');
     return template 'conversation.html', {domain => param('domain'), id => param('id')};
 };
 
 get '/gui/provider/:comma_separated_emails' => sub {
+	content_type('text/html');
 	my @emails = map { s/\s*(\S+)\s*/$1/; $_ } split ',', param('comma_separated_emails');
 	#return to_dumper \@emails;
     return template 'provider.html', {title=> 'Provider', domain => param('domain'), from => param('comma_separated_emails')};
+};
+
+get '/gui/send-batch' => sub {
+	content_type('text/html');
+    return template 'batch_send.html', {title=> 'Provider', domain => param('domain')};
 };
 
 
