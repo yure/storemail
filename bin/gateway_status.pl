4aeab20ba5c63f8136d650e280b112a8da2e5d7e
Grega Pompe <grega.pompe@informa.si>
2016-06-08 08:57:00 +0200

    Gateway status update

--- bin/gateway_status.pl
+++ bin/gateway_status.pl
@@ -5,6 +5,7 @@ use Try::Tiny;
 use StoreMail::Helper;
 use DateTime::Format::MySQL;
 use Dancer::Plugin::Email;
+use StoreMail::Gateway::NeogateTG;
 
 logfile('gateway_status');
 
@@ -12,7 +13,7 @@ sub error_msg {
 	my $gateway_id = shift;
 	my $settings = config->{gateways}->{$gateway_id};
 	my $status = shift;
-	my $msg = $settings->{host}. " PROBLEM. Status: ".to_json $status;
+        my $msg = $gateway_id.' '.$settings->{host}.':'.$settings->{port}. " PROBLEM. Status: ".to_json $status;
 	return $msg;
 }
 
@@ -32,29 +33,31 @@ my @errors;
 
 exit(0) if ( (localtime)[2] == 0 and (localtime)[1] < 3 );
 
-for my $gateway_id (keys config->{gateways}){	
-	my $settings = config->{gateways}->{$gateway_id};
+for my $gateway_id (keys config->{gateways}){
+	next unless config->{gateways}->{$gateway_id}->{type} eq 'neogate_tg';	
+	printt $gateway_id;
 	try{
-		
-		my $gateway = StoreMail::SMS->new( $settings );		
+	        my $settings = config->{gateways}->{$gateway_id};
+		my $gateway = StoreMail::Gateway::NeogateTG->new( $settings );		
 		#print to_json $gateway->check_status;
-		my $status = $gateway->check_status;
+		my $status = $gateway->check_status || {status => 'Empty response'};
 		my $ok_count = 0;
-		for my $sim_status (@{$status->{CMD}}){
-			$ok_count++ if index($sim_status, 'Power off' ) > -1; 
-			$ok_count++ if index($sim_status, 'Power on, Provisioned, Up, Active,Standard' ) > -1; 
+		if(@{$status->{CMD}}){
+			for my $sim_status (@{$status->{CMD}}){
+				$ok_count++ if index($sim_status, 'Power off' ) > -1; 
+				$ok_count++ if index($sim_status, 'Power on, Provisioned, Up, Active,Standard' ) > -1; 
+			}
+			$status->{port_count} = "$ok_count / ".$settings->{active_ports};
+			push @errors, [$gateway_id, $status] unless $ok_count == $settings->{active_ports};
 		}
-		push @errors, [$gateway_id, $status] unless $ok_count == $settings->{active_ports};
-		#email_alert($gateway_id, $status) unless $ok_count == $settings->{active_ports};
 	}
 	catch {
-		push @errors, [$gateway_id, {status => 'Timeout'}];
-		#email_alert($gateway_id, {status => 'Timeout'});
+		push @errors, [$gateway_id, {status => $_, }];
 	};
 }
 
 my $msg = join ("\n\n", map {error_msg(@$_)} @errors);
 
-email_alert($msg);
+email_alert($msg) if $msg;
 
 print ".";
