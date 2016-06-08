9d6e08b570a04f2a0f9cfe2c5a2e4405b546d371
gregapompe <grega.pompe@informa.si>
2016-06-08 13:20:33 +0200

    Port check

---
+++ bin/sms_send_port_check.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use Dancer ':script';
+use StoreMail::SMS;
+use Try::Tiny;
+use StoreMail::Helper;
+use Dancer::Plugin::DBIC qw(schema resultset rset);
+
+
+my $numbers = config->{phone_numbers};
+
+for my $number (keys %$numbers){
+	printt $number;
+	
+	my $sms = schema->resultset('SMS')->create({
+		direction => 'o',
+		domain => 'test.storemail.com',			
+		frm => $number, 
+		to => config->{admin_phone}, 
+		body => "Test from $number", 
+		send_queue => 1,
+    });
+}
