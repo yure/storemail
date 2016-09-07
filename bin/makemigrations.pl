03b182e36384a0d7083b5c51ee3730c6fdf57ffd
gregapompe <grega.pompe@informa.si>
2016-09-07 08:18:43 +0200

    force migrate

--- bin/makemigrations.pl
+++ bin/makemigrations.pl
@@ -3,15 +3,17 @@ use Dancer ':script';
 use Dancer::Plugin::DBIC qw(schema resultset rset);
 use DBIx::Class::DeploymentHandler;
 
+my $arg = {map {$_ => 1} @ARGV};
+
 my $dh = DBIx::Class::DeploymentHandler->new({ 
 	schema => schema, 
 	databases => 'MySQL',
-	sql_translator_args => { add_drop_table => 0 }, 
+	force_overwrite => $arg->{force},
+	sql_translator_args => { add_drop_table => 0, }, 
 });
  
-my ($from, $to) = @ARGV;
 
 $dh->prepare_deploy;
-$dh->prepare_upgrade({ from_version => $from, to_version => $to});
+$dh->prepare_upgrade();
 
 print "Done.\n"
